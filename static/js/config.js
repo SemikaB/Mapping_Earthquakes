302c50873b573baa7b8fc676ff8a0887be991493
SemikaB <100738128+SemikaB@users.noreply.github.com>
2022-06-19 21:08:28 -0400

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoic2J1cm5ldHRlIiwiYSI6ImNsNG0wNzVmdDFld2szZ2s3ZXB3d3Rvd3YifQ.pv-2eexcRzfVtVRfRBFs5A";
+const API_KEY = "pk.eyJ1Ijoic2J1cm5ldHRlIiwiYSI6ImNsNG0wNzVmdDFld2szZ2s3ZXB3d3Rvd3YifQ.pv-2eRzfVtVRfRBFs5A";
